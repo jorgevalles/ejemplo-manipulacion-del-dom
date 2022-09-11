886605107ebae8782f12e90259ead633054cc366
jorge <1jorgevalles@gmail.com>
2022-09-11 13:23:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btncalcular");
+
+function btnOnclick() {
+    const suma = Number(input1.value) + Number(input2.value)
+    console.log(suma);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log({
+//     h1,
+//     p,
+//     parrafito,
+//     pid,
+//     input,
+// })
+
+// h1.innerHTML = "jorge"
+// h1.innerText = "jorge valles"
+
+// const img = document.createElement("img");
+// img.setAttribute("src","https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg");
+// console.log(img);
+// pid.innerHTML ="":
+// pid.append(img);
